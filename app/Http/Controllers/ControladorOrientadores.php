7c47b585b2357567260e8fa549243e561e998d4b
joao-gino <79086821+joao-gino@users.noreply.github.com>
2021-10-18 20:25:27 -0300

    Update ControladorOrientadores.php

--- app/Http/Controllers/ControladorOrientadores.php
+++ app/Http/Controllers/ControladorOrientadores.php
@@ -9,7 +9,7 @@ class ControladorOrientadores extends Controller
 {
     public function listarAdvisors(Request $req) {
 
-        $advisors = DB::table('app_advisors')->get();
+        $advisors = DB::table('app_advisors')->leftjoin('app_users', 'app_users.id', 'app_advisors.id_user')->get();
 
         return response()->json(['data' => $advisors], 200);
     }
